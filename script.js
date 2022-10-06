78a672fc010a43ff28d1fbde938441039a481a7b
Achinit99 <achinit720@gmail.com>
2022-10-06 14:35:00 +0530

    Challenge

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 // Using Google, StackOverflow and MDN
 
+const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+
 const calcTempAmplitude = function (temps) {
   let max = temps[0];
+  let min = temps[0];
+
   for (let i = 0; i < temps.length; i++) {
-    if (temps[i] > max) max = temps[i];
+    const curTemp = temps[i];
+    if (typeof curTemp !== 'number') continue;
+
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
   }
-  console.log(max);
+  console.log(max, min);
+  return max - min;
 };
-calcTempAmplitude([3, 7, 9]);
+const amplitude = calcTempAmplitude(temperatures);
+console.log(amplitude);
