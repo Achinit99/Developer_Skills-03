8382a90373d006f28d1386d0f264f16a7d634b28
Achinit99 <achinit720@gmail.com>
2022-10-06 14:16:34 +0530

    Using Google, StackOverflow and MDN

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-// Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+// Using Google, StackOverflow and MDN
+
+const calcTempAmplitude = function (temps) {
+  let max = temps[0];
+  for (let i = 0; i < temps.length; i++) {
+    if (temps[i] > max) max = temps[i];
+  }
+  console.log(max);
+};
+calcTempAmplitude([3, 7, 9]);
